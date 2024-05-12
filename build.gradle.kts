d2c7f182cdab92666d0d47a32e271bc4dc87d572
amitbarel <amitbrl1797@gmail.com>
2024-05-12 18:29:44 +0300

    Firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,5 @@ buildscript {
 plugins {
     id("com.android.application") version "8.4.0" apply false
     id("com.android.library") version "8.4.0" apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
